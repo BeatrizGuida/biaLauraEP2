229956026d43727891eeb394972e8e1d504dfc9b
laurapittoli <lauravpp@al.insper.edu.br>
2025-10-20 17:35:45 -0300

    def_pos

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,10 @@
 #EX 1
+def define_posicoes(linha, coluna, orientacao,tamanho):
+    posicoes = []
+    if orientacao == 'horizontal':
+        for i in range(tamanho):
+            posicoes.append((linha, coluna + i))
+    elif orientacao == 'vertical':
+        for i in range(tamanho):
+            posicoes.append((linha + i, coluna))
+    return posicoes  
