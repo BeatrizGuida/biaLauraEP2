a48f9b257555fb475035a83e06d76c10c5d3742b
BeatrizGuida <beatrizbpg@al.insper.edu.br>
2025-10-20 17:49:43 -0300

    define_posicoes2

--- funcoes.py
+++ funcoes.py
@@ -6,20 +6,5 @@ def define_posicoes(linha, coluna, orientacao,tamanho):
             posicoes.append([linha, coluna + i])
     elif orientacao == 'vertical':
         for i in range(tamanho):
-<<<<<<< HEAD
-            posicoes.append((linha + i, coluna))
-    return posicoes  
-
-
-def preenche_frota (dicio_frota, nome_navio, linha, coluna, orientacao, tamanho):
-    lista= define_posicoes(linha, coluna, orientacao, tamanho)
-    if nome_navio in dicio_frota:
-        dicio_frota[nome_navio] += lista
-    else:
-        dicio_frota[nome_navio]= lista
-
-    return dicio_frota
-=======
             posicoes.append([linha + i, coluna])
     return posicoes  
->>>>>>> 0065d6562a5202dc857956fbc90484347f024230
