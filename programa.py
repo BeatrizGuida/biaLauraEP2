01c8fa763a8d6ba5900a3a0f358deb8704299f35
BeatrizGuida <beatrizbpg@al.insper.edu.br>
2025-10-23 13:30:52 -0300

    prograMA3

--- programa.py
+++ programa.py
@@ -1,41 +1,43 @@
 from funcoes import posicao_valida, define_posicoes, preenche_frota
 
-lista_embarcacoes = ['porta-aviões', 'navio-tanque', 'contratorpedeiro', 'submarino']
-lista_tamanho = [4, 3, 2, 1]
+embarcacoes = [
+    ("porta-aviões", 4, 1),
+    ("navio-tanque", 3, 2),
+    ("contratorpedeiro", 2, 3),
+    ("submarino", 1, 4)
+]
 
-dicio_frota = {
+frota = {
     "porta-aviões": [],
     "navio-tanque": [],
     "contratorpedeiro": [],
     "submarino": [],
 }
 
-for i in range(len(lista_embarcacoes)):
-    nome = lista_embarcacoes[i]
-    tamanho = lista_tamanho[i]
-
-    print(f'Insira as informações referentes ao navio {nome} que possui tamanho {tamanho}')
-    
-
-    while True:  
-        linha = int(input('Linha: '))
-        coluna = int(input('Coluna: '))
-
-        if nome != 'submarino':
-            orientacao_num = int(input('Orientação (1 - vertical, 2 - horizontal): '))
-            if orientacao_num == 1:
-                orientacao = 'vertical'
-            elif orientacao_num == 2:
-                orientacao = 'horizontal'
+for nome, tamanho, quantidade in embarcacoes:
+    for _ in range(quantidade):
+        print(f'Insira as informações referentes ao navio {nome} que possui tamanho {tamanho}')
+        
+        while True:  
+            linha = int(input('Linha: '))
+            coluna = int(input('Coluna: '))
             
-        else:
-            orientacao = None  
+            if nome != 'submarino':
+                orientacao_num = int(input('Orientação (1 - vertical, 2 - horizontal): '))
+                if orientacao_num == 1:
+                    orientacao = 'vertical'
+                elif orientacao_num == 2:
+                    orientacao = 'horizontal'
+                else:
+                    print('Orientação inválida! Digite 1 ou 2.')
+                    continue
+            else:
+                orientacao = None  
+            
+            if posicao_valida(frota, linha, coluna, orientacao, tamanho):
+                frota = preenche_frota(frota, nome, linha, coluna, orientacao, tamanho)
+                break
+            else:
+                print('Esta posição não está válida! Tente novamente.')
 
-        if posicao_valida(dicio_frota, linha, coluna, orientacao, tamanho):
-            dicio_frota = preenche_frota(dicio_frota, nome, linha, coluna, orientacao, tamanho)
-            break  
-        else:
-            print('Esta posição não está válida! Tente novamente.')
-
-print(dicio_frota)
-
+print(frota)
